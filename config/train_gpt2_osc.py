03f4ff0aec252ac7b14cdb4bc42034c69088bcf6
aaron-jencks <aaronjencks@aol.com>
2025-01-29 16:51:56 -0500

    updating batch size

--- config/train_gpt2_osc.py
+++ config/train_gpt2_osc.py
@@ -1,2 +1,6 @@
-gradient_accumulation_steps = 42
-batch_size = 16
+# TODO these need to multiple to 60 * 8 = 480
+# modify batch_size
+# schedule all of the values at once
+# 1x60, 2x30, 3x20, 4x15, etc...
+batch_size = 60
+gradient_accumulation_steps = 8
